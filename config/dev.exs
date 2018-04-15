c5b4ea4b1c09ab1a87b779e1b5231354d6515d99
George Karmelich <george.karmelich@gmail.com>
2018-04-15 17:19:08 -0400

    add map

--- config/dev.exs
+++ config/dev.exs
@@ -51,8 +51,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :captureCampus, CaptureCampus.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "capturecampus",
-  password: "postgres",
-  database: "capturecampus_dev",
+  username: "capture",
+  password: "Password",
+  database: "capture_dev",
   hostname: "localhost",
   pool_size: 10
