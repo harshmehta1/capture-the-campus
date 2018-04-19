bcaab85ddab25ccf29b0145056824197c70a7d19
harshmehta1 <harshsmehta1@gmail.com>
2018-04-19 00:57:35 -0400

    minor fixes

--- lib/captureCampus/game.ex
+++ lib/captureCampus/game.ex
@@ -9,11 +9,10 @@ defmodule CaptureCampus.Game do
       buildings: Enum.take_random(buildingList(), game_size + 1),
       team1Attacks: [],
       team2Attacks: [],
+      team1Score: [],
+      team2Score: [],
       status: "Waiting For Players",
       winner: "",
-      team1BuildingsCaptured: [],
-      team2BuildingsCaptured: [],
-
     }
   end
 
@@ -26,10 +25,11 @@ defmodule CaptureCampus.Game do
       buildings: game.buildings,
       team1Attacks: game.team1Attacks,
       team2Attacks: game.team2Attacks,
+      team1Score: game.team1Score,
+      team2Score: game.team2Score,
       status: game.status,
       winner: game.winner,
-      team1BuildingsCaptured: game.team1BuildingsCaptured,
-      team2BuildingsCaptured: game.team2BuildingsCaptured,
+
     }
   end
 
@@ -42,24 +42,24 @@ defmodule CaptureCampus.Game do
       buildings: Map.get(game, "buildings"),
       team1Attacks: Map.get(game, "team1Attacks"),
       team2Attacks: Map.get(game, "team2Attacks"),
+      team1Score: Map.get(game, "team1Score"),
+      team2Score: Map.get(game, "team2Score"),
       status: Map.get(game, "status"),
       winner: Map.get(game, "winner"),
-      team1BuildingsCaptured: Map.get(game, "team1BuildingsCaptured"),
-      team2BuildingsCaptured: Map.get(game, "team2BuildingsCaptured"),
     }
   end
 
   def buildingList() do
-    [%{:name => "West Village H", :lat => 42.33857, :lng => -71.092355, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "" },
-      %{:name => "Dodge Hall", :lat => 42.340324, :lng => -71.08785, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "" },
-      %{:name => "Marino Center", :lat => 42.340272, :lng => -71.090269, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "" },
-      %{:name => "ISEC", :lat => 42.337733, :lng => -71.086912, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "" },
-      %{:name => "Ryder Hall", :lat => 42.336605, :lng => -71.090850, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "" },
-      %{:name => "MFA", :lat => 42.339381, :lng => -71.094048, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "" },
-      %{:name => "Matthews Arena", :lat => 42.341235, :lng => -71.084523, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => ""},
-      %{:name => "Internation Village", :lat => 42.335102, :lng => -71.089176, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => ""},
-      %{:name => "Shillman Hall", :lat => 42.337553, :lng => -71.090191, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "" },
-      %{:name => "East Village", :lat => 42.340437, :lng => -71.086879, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "" }]
+    [%{:name => "West Village H", :lat => 42.33857, :lng => -71.092355, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => "" },
+      %{:name => "Dodge Hall", :lat => 42.340324, :lng => -71.08785, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => "" },
+      %{:name => "Marino Center", :lat => 42.340272, :lng => -71.090269, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => "" },
+      %{:name => "ISEC", :lat => 42.337733, :lng => -71.086912, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => "" },
+      %{:name => "Ryder Hall", :lat => 42.336605, :lng => -71.090850, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => "" },
+      %{:name => "MFA", :lat => 42.339381, :lng => -71.094048, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => "" },
+      %{:name => "Matthews Arena", :lat => 42.341235, :lng => -71.084523, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => ""},
+      %{:name => "Internation Village", :lat => 42.335102, :lng => -71.089176, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => ""},
+      %{:name => "Shillman Hall", :lat => 42.337553, :lng => -71.090191, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => "" },
+      %{:name => "East Village", :lat => 42.340437, :lng => -71.086879, :captured => false, :underAttack => false, :attacker => %{}, :attackEnds => "", :owner => "" }]
   end
 
   def handleAttack(game, building, team) do
@@ -112,25 +112,39 @@ defmodule CaptureCampus.Game do
     game
   end
 
-  def removePlayer(game, user_id) do
-    team1 = Map.get(game, "team1")
-    newTeam1 = Enum.filter(team1, fn(x) -> Map.get(x, "user_id")!=user_id end)
-    if (Map.get(game, "status") == "start") && (length(newTeam1) == 0) do
-      game = Map.put(game, "status", "Game Over!")
-      game = Map.put(game, "winner", "Team 2")
-    end
-    team2 = Map.get(game, "team2")
-    newTeam2 = Enum.filter(team2, fn(y) -> Map.get(y, "user_id")!=user_id end)
-    if (Map.get(game, "status") == "start") && (length(newTeam2) == 0) do
-      game = Map.put(game, "status", "Game Over!")
-      game = Map.put(game, "winner", "Team 1")
-    end
-    game = Map.replace!(game, "team1", newTeam1)
-    game = Map.replace!(game, "team2", newTeam2)
 
-    IO.inspect("REMOVE")
-    IO.inspect(game)
-    game
+  def addPlayer(user_id, state) do
+    team1 = state.team1
+    team2 = state.team2
+    if length(team1) < 2 do
+      team1 = team1 ++ [user_id]
+      state = Map.put(state, :team1, team1)
+    else
+      team2 = team2 ++ [user_id]
+      state = Map.put(state, :team2, team2)
+    end
+    state
+  end
+
+  def removePlayer(game, user_id) do
+  team1 = Map.get(game, "team1")
+   newTeam1 = Enum.filter(team1, fn(x) -> Map.get(x, "user_id")!=user_id end)
+   if (Map.get(game, "status") == "start") && (length(newTeam1) == 0) do
+     game = Map.put(game, "status", "Game Over!")
+     game = Map.put(game, "winner", "Team 2")
+   end
+   team2 = Map.get(game, "team2")
+   newTeam2 = Enum.filter(team2, fn(y) -> Map.get(y, "user_id")!=user_id end)
+   if (Map.get(game, "status") == "start") && (length(newTeam2) == 0) do
+     game = Map.put(game, "status", "Game Over!")
+     game = Map.put(game, "winner", "Team 1")
+   end
+   game = Map.replace!(game, "team1", newTeam1)
+   game = Map.replace!(game, "team2", newTeam2)
+
+   IO.inspect("REMOVE")
+   IO.inspect(game)
+   game
   end
 
 end
