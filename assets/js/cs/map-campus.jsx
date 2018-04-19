1c614e9d831cc681d6ffd1677df556321250d0fe
George Karmelich <george.karmelich@gmail.com>
2018-04-19 00:47:55 -0400

    update flag display on map

--- assets/js/cs/map-campus.jsx
+++ assets/js/cs/map-campus.jsx
@@ -71,13 +71,13 @@ function CamMap(props){
     if(x.underAttack && (x.attacker.team == "team1") && (x.captured == false)){
       return <Marker position={{lat: x.lat, lng: x.lng }} title={x.name} key={ii} animation={google.maps.Animation.BOUNCE} icon={"/images/greenflag.png"}/>
     }
-    if(x.captured && (x.attacker.team == "team1")){
+    if(x.captured && (x.owner == "team1")){
       return <Marker position={{lat: x.lat, lng: x.lng }} title={x.name} key={ii} icon={"/images/greenflag.png"}/>
     }
     if(x.underAttack && (x.attacker.team == "team2") && (x.captured == false)){
       return <Marker position={{lat: x.lat, lng: x.lng }} title={x.name} key={ii} animation={google.maps.Animation.BOUNCE} icon={"/images/redflag.png"}/>
     }  
-    if(x.captured && (x.attacker.team == "team2")){
+    if(x.captured && (x.owner == "team2")){
       return <Marker position={{lat: x.lat, lng: x.lng }} title={x.name} key={ii} icon={"/images/redflag.png"}/>
     }  
     else {
